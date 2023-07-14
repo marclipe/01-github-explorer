974f4299a207e8bd110a892b099dbf107b2c0cf5
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-14 16:42:58 -0300

    Renamed components for React.

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,6 +1,14 @@
 import '../styles/repositories.scss'
 
-export function RepositoryItem(props) {
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem(props: RepositoryItemProps) {
   return (
     <li>
       <strong>{props.repository.name}</strong>
